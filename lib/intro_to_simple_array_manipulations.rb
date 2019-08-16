596a785beb546c07ebde576dedd78904e68111d0
Richard Huelves <rhuelves@gmail.com>
2019-08-16 17:37:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,10 +22,11 @@ def using_shift (my_favorite_cities)
 end
 
 def shift_with_args (ice_cream_brands)
-  brands_removed = 
+  brands_removed = ice_cream_brands.shift(2)
 end
 
 def using_concat
+  
 end
 
 def using_insert
