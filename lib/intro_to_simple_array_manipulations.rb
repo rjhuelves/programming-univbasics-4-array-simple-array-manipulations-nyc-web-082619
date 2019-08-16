24cb8bd2e79fd5cadc1f2d275ab1205cb54aab80
Richard Huelves <rhuelves@gmail.com>
2019-08-16 17:55:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -29,8 +29,8 @@ def using_concat (my_favorite_things, more_favs)
   all_my_favs = my_favorite_things.concat(more_favs)
 end
 
-def using_insert (list_of_programming_languages, )
-  new_array = list_of_programming_languages.insert()
+def using_insert (list_of_programming_languages, another_language )
+  new_array = list_of_programming_languages.insert(4, another_language)
 end
 
 def using_uniq
