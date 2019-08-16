1c087ab1f749efc9eccd6329abf4272dbe61c0b5
Richard Huelves <rhuelves@gmail.com>
2019-08-16 18:10:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -45,8 +45,8 @@ def using_delete (instructors)
   no_offense_steven = instructors.delete("Steven")
 end
 
-def using_delete_at
-  
+def using_delete_at (famous_robots)
+  delete_robot = famous_robots.delete[2]
 end
 
 
